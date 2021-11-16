d1a921d44ec53c7b229679eb375f0b5230dd6b08
newton1057 <newton1057@gmail.com>
2021-11-16 00:07:15 -0600

    3.0

--- Archivo1.jl
+++ Archivo1.jl
@@ -8,16 +8,31 @@
 #Multiplicacion *
 #Division /
 
+#Instruccion @show sirver para imprimir la Instruccion 
 #Enteros
 xe = 1
 ye = 2
-ze = xe + ye
+@show ze = xe + ye
 println(ze)
+
 
 #Float o Decimales
 xf = 1.05
 yf = 2.55
-@show ze = xf + yf 
-println(ze) 
+@show zf = xf + yf 
+println(zf) 
 
+
+#typeof sirve para saber que tipo de Dato Enteros
+typeof(zf)
+
+
+#Strings // Cadenas de Texto
+
+#Declaracion
+a = "Hola"
+b = "Alejandra"
+
+#Concatenacion * Solo Strings
+println(a * " " * b)
 
--- Archivo2.jl
+++ Archivo2.jl
@@ -1,6 +1,9 @@
+#Estructuras de datos
 
-#Strings // Cadenas de Texto
-a = "Hola"
-b = "Alejandra"
+#Tuplas
 
-println(a * " " * b)
+tupla_x = ( x = 1, y = 2, z = 3, a = "Nombre")
+
+
+@show tupla_x;
+tupla_x
