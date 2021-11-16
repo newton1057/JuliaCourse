d72a6baa244f2b87bcd84605c86c12d7ccd8a9df
newton1057 <newton1057@gmail.com>
2021-11-15 23:19:50 -0600

    v1.0

---
+++ Archivo1.jl
@@ -0,0 +1,18 @@
+#Tipos de Variable
+#Enteros
+xe = 1
+ye = 2
+ze = xe + ye
+println(ze)
+
+#Float o Decimales
+xf = 1.05
+yf = 2.55
+@show ze = xf + yf 
+println(ze) 
+
+#Strings // Cadenas de Texto
+a = "Hola"
+b = "Alejandra"
+
+println(a * " " * b)
